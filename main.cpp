c6a350095de82b8098d21e7c56445cbe4fe5c37e
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2023-11-14 19:35:43 +0000

    Entrega final

--- main.cpp
+++ main.cpp
@@ -1,56 +1,68 @@
 #include "mbed.h"
 #include <iostream>
+#include <chrono>
+#include <cmath>
+#include "TextLCD.h"
 
 UnbufferedSerial serial(USBTX, USBRX, 9600);
+TextLCD display(D2,D3,D4,D5,D6,D7,TextLCD::LCD16x2);
 
 AnalogIn ain(A1);
 AnalogIn aout(A2);
 
-using namespace std::chrono;
-Timer t;
-const int distancia = 1;
-const double precision = 0.4;
+const double distancia = 0.14;
 
+using namespace std;
+using namespace chrono;
+
+
+void escribirLCD(double a) {
+    display.cls();
+    display.locate(0, 0);
+    display.printf("aceleracion: ");
+    display.locate(0, 1);
+    display.printf("%f", a);
+}
 
 bool sensor(AnalogIn fotoSensor) {
-    return fotoSensor.read()>precision;
+    return fotoSensor.read()==1;
 }
 
 int main()
 {
-    double aceleracion, tiempo;
+    Timer t;
+    string mensaje;
+    double aceleracion;
 
     while (true) {
-        printf("Compuerta 1 \n");
+        t.reset();
+
+        cout << "Start..."<< endl;
         // Espera hasta que pase algo por la primera compuerta
         while (sensor(ain)) {};
 
         // Inicializa el contador
         t.start();
-        printf("Inicia contador \n");
+        cout << "Inicia contador"<< endl;
 
         // Espera hasta que pase algo por la segunda compuerta
         while (sensor(aout)) {};
 
         // Detiene el contador
         t.stop();
-        printf("Fin contador \n");
 
-        // Mostrar el tiempo por pantalla
-        tiempo = duration_cast<milliseconds>(t.elapsed_time()).count();
+        // Obtener el tiempo transcurrido
+        long long tiempo = duration_cast<milliseconds>(t.elapsed_time()).count();
+
+        // Pasar de milisegundos a segundos
+        double tiempo_s = tiempo/1000.0;
 
         // Calcular aceleracion
-        aceleracion = 2*distancia/pow(tiempo, 2);
+        aceleracion = 2*distancia/pow(tiempo_s, 2);
 
-        printf("Tiempo: %f\n", tiempo);
-        printf("Aceleracion: %f\n", aceleracion);
+        escribirLCD(aceleracion);
 
-        t.reset();
+        cout << "Tiempo: " << tiempo_s << endl;
+        cout << "Aceleracion: " << aceleracion << endl;
     }
 }
-/*
-int main() {
-    while (true) {
-        std::cout << ain.read() << endl;
-    }
-}*/
